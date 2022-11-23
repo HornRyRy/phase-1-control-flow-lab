6e98c5a41cae3175a2011f32201bcde05b280da3
Ryan Horn <rj.horn@gmail.com>
2022-11-23 09:52:52 -0700

    commit tree 778760587a735e06669af61b530fbec8ae967383

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDist){
+  if (rideDist <= 400){
+    return 'This one is on me!'
+  }
+
+  else if(rideDist > 400 && rideDist <= 2000){
+    return 'That will be twenty bucks.'
+  }
+
+  else if(rideDist > 2000 && rideDist <= 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+
+  else if(rideDist > 2500){
+    return 'No can do.'
+  }
+
+  else{
+    return 'You are out of bounds, buddy. Not a valid input'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destIsNyc){
+  return (destIsNyc === 'NYC' ? 'Ok, sounds good.' : 'No go.') 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipIsGenerous){
+  if(tipIsGenerous === 'generous'){
+    return "Thank you so much."
+  }
+
+  else if(tipIsGenerous === 'not as generous'){
+    return 'Thank you.'
+  }
+
+  else if(tipIsGenerous === 'LOTR'){
+    return 'Not all those who wander are lost'
+  }
+
+  else{
+    return 'Bye.'
+  }
 }
+
+//Tests shown below as REFERENCE
+// describe('switchOnCharmFromTip()', function () {
+//   it('should return "Thank you so much." if the tip is generous', function () {
+//     expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+//   });
+
+//   it('should return "Thank you." if the tip is not as generous', function () {
+//     expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+//   });
+
+//   it('should return "Bye." if anything else', function () {
+//     expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+//   });
+// });
+// });
